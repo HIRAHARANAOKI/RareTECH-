a84bb491d2faeb1bf3a8500b5d412eaecd6f6f63
n-hirahara <genenao21@gmail.com>
2022-07-18 16:29:32 +0900

    fix: data fetch

--- weather-app/main.js
+++ weather-app/main.js
@@ -1,14 +1,14 @@
-// TODO 今日の日付を取得
+// 今日の日付を取得
 const date = new Date();
 
-// TODO 日付をHTMLに表示
+// 日付をHTMLに表示
 document.getElementById("today").innerHTML =
   dayjs(date).format("YYYY年MM月DD日");
 
-// TODO APIキーを入力
+// APIキーを入力
 const API_KEY = "72ebb0ee87d18e08a49736b76195b07d";
 
-// TODO 今日の天気を取得
+// 今日の天気を取得
 const getWeather = async () => {
   const res = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?lat=33.590&lon=130.419&appid=${API_KEY}`
@@ -17,11 +17,12 @@ const getWeather = async () => {
   return data;
 };
 
+// 画面をロードした時に天気情報を取得
 addEventListener("load", async () => {
   const weatherData = await getWeather();
 
   if (weatherData) {
-    console.log(weatherData, "name");
+    console.log(weatherData);
 
     document.getElementById("place").innerHTML = weatherData.name;
 

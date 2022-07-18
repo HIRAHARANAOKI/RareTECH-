785c9913f4bc47dae964331a2c258dd82883e4a1
n-hirahara <genenao21@gmail.com>
2022-07-18 16:24:57 +0900

    add: weather sample app

---
+++ weather-app/main.js
@@ -0,0 +1,30 @@
+// TODO 今日の日付を取得
+const date = new Date();
+
+// TODO 日付をHTMLに表示
+document.getElementById("today").innerHTML =
+  dayjs(date).format("YYYY年MM月DD日");
+
+// TODO APIキーを入力
+const API_KEY = "72ebb0ee87d18e08a49736b76195b07d";
+
+// TODO 今日の天気を取得
+const getWeather = async () => {
+  const res = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=33.590&lon=130.419&appid=${API_KEY}`
+  );
+  const data = await res.json();
+  return data;
+};
+
+addEventListener("load", async () => {
+  const weatherData = await getWeather();
+
+  if (weatherData) {
+    console.log(weatherData, "name");
+
+    document.getElementById("place").innerHTML = weatherData.name;
+
+    document.getElementById("weather").innerHTML = weatherData.weather[0].main;
+  }
+});
